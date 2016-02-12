787007850057aee2c974e58fb4dfc0fa4d15ab03
Kostas Georgakopoulos <kostas.georgakopoulos@rackspace.co.uk>
2016-02-12 08:45:33 +0000

    Added CentOS 6.7 platform in unit tests

--- test/unit/spec/rackspace_users_test_centos67_spec.rb
+++ test/unit/spec/rackspace_users_test_centos67_spec.rb
@@ -1,9 +1,9 @@
 require_relative 'spec_helper'
-require_relative 'centos66_options'
+require_relative 'centos67_options'
 
-describe 'rackspace_users_test::default on Centos 6.6' do
+describe 'rackspace_users_test::default on Centos 6.7' do
   cached(:chef_run) do
-    ChefSpec::SoloRunner.new(CENTOS66_SERVICE_OPTS) do |node|
+    ChefSpec::SoloRunner.new(CENTOS67_SERVICE_OPTS) do |node|
       stub_all(node)
     end.converge('rackspace_users_test::default')
   end
